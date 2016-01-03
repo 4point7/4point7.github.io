4ddc78cf1b877d397148b151a765099ce07b90de
ɯoɔ˙ɔᴉƃᴉpoɹd@ʎǝʌɐp <davey@prodigic.com>
2016-01-03 13:20:49 -0800

    popstate back button handler

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-$("a[href^=#]").on("click", function(e) {
+$(".portfolio-link").on("click", function(e) {
     e.preventDefault();
-    history.pushState({}, "", this.href);
+    history.pushState({view:"POPUP"}, "", "");
+});
+
+window.addEventListener("popstate", function(e) {
+	// return to home state
+	if (e.state.view == "POPUP") {
+		history.pushState({}, "", "/");
+    $(".close-modal").click();
+	}
 });
